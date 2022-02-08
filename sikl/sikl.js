459ab81e6064c997bbcb9faa0ebd38393fee0fe1
Diyor015 <jacobfugger2@gmail.com>
2022-02-08 15:58:03 +0500

    beginning js

---
+++ sikl/sikl.js
@@ -0,0 +1,12 @@
+let year = prompt('Daraxtdagi maymunlar sonini kiriting', '');
+
+if (year < 11) {
+  alert( 'Daraxtda' + year + 'ta maymunlar bor' );
+} else if (year < 16) {
+  alert('Daraxtda' + year + 'ta maymunchalar bor');
+} else if (year < 21){
+    alert( 'Daraxtda' + year + 'ta gorillalar bor' );
+}
+ else  {
+  alert( 'Xato!!!!!!' );
+}
---
+++ sikl/sikl2.js
@@ -0,0 +1,12 @@
+let year = prompt('Daraxtdagi maymunlar sonini kiriting', '');
+
+if (year < 11) {
+    console.log( 'Daraxtda ' + year + 'ta maymunlar bor' );
+} else if (year < 16) {
+    console.log('Daraxtda ' + year + 'ta maymunchalar bor');
+} else if (year < 21){
+    console.log( 'Daraxtda ' + year + 'ta gorillalar bor' );
+}
+ else  {
+  console.log( 'Xato!!!!!!' );
+}
